7b9f11ec6551b7e5abf9eb4812534fb046caaec2
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2024-06-20 15:20:23 +0700

    feat(configure): UP-10 configure vite build as modular, postcss and stylelint

--- src/components/Hello.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Hello: React.FC = () => {
-  return <div>Hello here from ui library</div>;
-};
-
-export default Hello;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,57 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve, relative, extname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { globSync } from "glob";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      exclude: ["src/**/*.mdx", "src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      input: Object.fromEntries(
+        globSync(["src/components/**/index.tsx", "src/main.ts"]).map((file) => {
+          // This remove `src/` as well as the file extension from each
+          // file, so e.g. src/nested/foo.js becomes nested/foo
+          const entryName = relative(
+            "src",
+            file.slice(0, file.length - extname(file).length),
+          );
+          // This expands the relative paths to absolute paths, so e.g.
+          // src/nested/foo becomes /project/src/nested/foo.js
+          const entryUrl = fileURLToPath(new URL(file, import.meta.url));
+          return [entryName, entryUrl];
+        }),
+      ),
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "assets/[name][extname]",
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+    emptyOutDir: true,
+  },
+  publicDir: false,
+});

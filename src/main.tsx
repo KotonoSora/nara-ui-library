5ce907713a9af4dbe4f5dcb5720d3feb579f9860
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2024-07-03 20:54:34 +0700

    feat(configure): UP-9 configure ui-library build type es module

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
 export { MyButton } from "@/components/MyButton";
 export { MyTitle } from "@/components/MyTitle";
 export { Typography } from "@/components/Typography";
+export { Logo } from "@/components/Logo";

3958a60e138fdfc9ea9b54a44a7abe9f92ca647e
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2024-07-13 08:55:47 +0700

    feat(configure): UP-11 upgrade storybook version

--- src/components/Logo/index.stories.ts
+++ src/components/Logo/index.stories.ts
@@ -4,7 +4,7 @@ import { Logo } from "./index";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Example/Logo",
+  title: "Components/Logo",
   component: Logo,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -13,15 +13,9 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    theme: {
-      control: "select",
-    },
-  },
+  argTypes: {},
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: {
-    theme: "default",
-  },
+  args: {},
 } satisfies Meta<typeof Logo>;
 
 export default meta;

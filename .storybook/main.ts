b7c49e5db3f3631324bde04ef06b0f491ea236dc
KotonoSora <26807417+KotonoSora@users.noreply.github.com>
2024-12-31 00:40:40 +0700

    feat: 🎸 Config storybook with themes with tailwincss

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-onboarding', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,12 +2,30 @@ import '#core/infrastructure/modern-normalize/global.css'
 import '#core/infrastructure/tailwindcss/global.css'
 import '#core/infrastructure/custom-css/global.css'
 
+import { withThemeByClassName, withThemeByDataAttribute } from '@storybook/addon-themes'
 import { reactRouterParameters, withRouter } from 'storybook-addon-remix-react-router'
 
 import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
-  decorators: [withRouter],
+  decorators: [
+    withRouter,
+    withThemeByClassName({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-mode',
+    }),
+  ],
   parameters: {
     reactRouter: reactRouterParameters({}),
     controls: {
